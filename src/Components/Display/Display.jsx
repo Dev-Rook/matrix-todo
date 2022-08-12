1072de20a573f3e6442de78156e3d98804096446
Dev Rook <110010478+Dev-Rook@users.noreply.github.com>
2022-08-12 13:55:21 -0500

    Functions need to be exported

--- src/Components/Display/Display.jsx
+++ src/Components/Display/Display.jsx
@@ -1,22 +1,26 @@
-import React from 'react'
+import React from "react";
 
-import styles from "./Styles/Display.module.css"
+import styles from "./Styles/Display.module.css";
 
 const Display = () => {
   return (
     <div className={styles.Display}>
-        <div className={styles.Input_Counter}>
-            <p className={styles.Count}>
-                #:10
-            </p>
-        </div>
-        <div className={styles.Date_Box}>
-        <p className={styles.Date}>
-                10.04.2022
-            </p>
-        </div>
-    </div>
-  )
-}
+      <div className={styles.Input_Counter}>
+        <p className={styles.Count}>#:10</p>
+      </div>
+      <div className={styles.Date_Box}>
+        <p className={styles.Date}>10.04.2022</p>
+      </div>
 
-export default Display
+      <div className={styles.Task_List}>
+        <div className={styles.Task}>
+          <img src={require("../../Assets/Icons/Task-Icon.png")} alt="" className={styles.Task_Icon} />
+          <p className={styles.The_Task}>Dog Water</p>
+          <img src={require("../../Assets/Icons/X-Icon.png")} alt="" className={styles.Delete_Icon} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Display;
--- src/Views/Main/Main.jsx
+++ src/Views/Main/Main.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 
 import styles from "./Styles/Main.module.css";
 
@@ -7,6 +7,29 @@ import Display from "../../Components/Display/Display";
 import Footer from "../../Components/Footer/Footer";
 
 const Main = () => {
+
+    const [taskList, setTaskList] = useState([]);
+    const [newTask, SetNewTask] = useState("");
+
+    const handleChange = (e) => {
+        if (e.key === "Enter") {
+            setTaskList(e.taret.value);
+        }
+    };
+
+    const addTask = () => {
+        const task = {
+            id: taskList.length === 0 ? 1 : taskList[taskList.length -1].id + 1,
+            taskName: newTask
+        };
+
+        setTaskList([...taskList, task])
+    }
+
+    const deleteTask = (id) => {
+        setTaskList(taskList.filter((task) => task.id !== id ));
+    }
+
   return (
     <div className={styles.Main}>
       <div className={styles.Container}>
